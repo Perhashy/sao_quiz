0e9fcb255da2d7c13f19b4cd3eaf451b35ad3600
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-06-05 23:47:54 +0900

    ファイル作成、html初期記述

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>SAOクイズ</title>
+    <link rel="stylesheet" href="styles.scss">
+  </head>
+  <body>
+    <h1>SAOクイズ!</h1>
+  </body>
+</html>
