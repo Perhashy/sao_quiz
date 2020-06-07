9efdab6dbfbcb5c73f1854163a2918ef09dbd13e
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-06-07 21:47:49 +0900

    正しい答えを受け取りjson型に変換

--- _answer.php
+++ _answer.php
@@ -4,3 +4,9 @@ require_once(__DIR__ . '/config.php');
 require_once(__DIR__ . '/Quiz.php');
 
 $quiz = new MyApp\Quiz();
+$correctAnswer = $quiz->correctAnswer();
+
+header('content-type: application/json; charset=UTF-8');
+echo json_encode([
+  'correct_answer' => $correctAnswer
+]);
