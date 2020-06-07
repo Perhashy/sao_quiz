67f0b02d0783b2a4989e1c5dbdfd15badfc87e3c
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-06-07 22:10:26 +0900

    スタイルのためのクラス付与

--- quiz.js
+++ quiz.js
@@ -3,12 +3,20 @@ $(function() {
 
   $('.answer').on('click', function(){
     var $selected = $(this);
+    $selected.addClass('selected');
     var answer = $selected.text();
     console.log(answer);
 
     $.post('/_answer.php', {
       
     }).done(function(res) {
+      $('answer').each(function(){
+        if ($(this).text() === res.correct_answer) {
+          $(this).addClass('correct');
+        } else {
+          $(this).addClass('wrong');
+        }
+      });
       if (answer === res.correct_answer) {
         $selected.text(answer + ' ...正解！👍');
       } else {
